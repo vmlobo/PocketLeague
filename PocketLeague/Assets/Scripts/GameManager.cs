9702a7d72504e9cd7c50a1813637ee356baa95e6
Vitor Lobo <vitormarqueslobo@gmail.com>
2019-10-13 04:54:12 +0100

    Random Goals

--- PocketLeague/Assets/Scripts/GameManager.cs
+++ PocketLeague/Assets/Scripts/GameManager.cs
@@ -10,9 +10,9 @@ public class GameManager : MonoBehaviour
     private GameObject ball;
 
     public GameObject goalPrefab;
+
     private GameObject goalPlayer1;
     private GameObject goalPlayer2;
-
 
     //public Rigidbody rb;
     //private CarController cr;
@@ -27,30 +27,16 @@ public class GameManager : MonoBehaviour
 
     private void Start()
     {
-        goalPlayer1 = Instantiate(goalPrefab, new Vector3(-12.0f, Random.Range(-11.87f, 8.57f), 0.0f), Quaternion.identity);
-        goalPlayer2 = Instantiate(goalPrefab, new Vector3(18.9f, Random.Range(-11.87f, 8.57f), 0.0f), Quaternion.identity);
-        ball = Instantiate(prefabBola, new Vector3(0.0f, 0.0f, 0.0f), Quaternion.identity);
+        goalPlayer1 = Instantiate(goalPrefab, new Vector3(-11.33f, Random.Range(-3.38f, 4.20f), 0.0f), Quaternion.identity);
+        goalPlayer2 = Instantiate(goalPrefab, new Vector3(11.33f, Random.Range(-3.38f, 4.20f), 0.0f), Quaternion.identity);
+        ball = Instantiate(prefabBola, new Vector3(0.0f, 5.0f, 0.0f), Quaternion.identity);
         player1pos = player1.transform.position;
         player2pos = player2.transform.position;
         ballpos = ball.transform.position;
-        //CarController cc = GetComponent(typeof(CarController)) as CarController;
 
     }
 
-    private void OnCollisionEnter(Collision collision)
-    {
-        if (collision.gameObject == goalPlayer2)
-        {
-            Goal(player1);
-
-        }
-        else if (collision.gameObject == goalPlayer1)
-        {
-            Goal(player2);
-        }
-    }
-
-    private void Goal(GameObject player)
+    public void Goal(GameObject player)
     {
         
         if (player.gameObject.name == "Player1")
@@ -92,21 +78,24 @@ public class GameManager : MonoBehaviour
         Debug.Log("Pausing");
 
 
-        Destroy(ball);
-        Destroy(goalPlayer1);
-        Destroy(goalPlayer2);
-        //cc.enable = false;
-        yield return new WaitForSeconds(3);
+        Destroy(ball);//TODO explosao bola
+        
+
+        Time.timeScale = 0.5f;
+
+        yield return new WaitForSeconds(1.5f);
+
+        Time.timeScale = 1f;
+
         ball = Instantiate(prefabBola, new Vector3(0.0f, 0.0f, 0.0f), Quaternion.identity);
 
-        goalPlayer1 = Instantiate(goalPrefab, new Vector3(-12.0f, Random.Range(-11.87f, 8.57f), 85), Quaternion.identity);
-        goalPlayer2 = Instantiate(goalPrefab, new Vector3(18.9f, Random.Range(-11.87f, 8.57f), 85), Quaternion.identity);
+        Destroy(goalPlayer1);
+        Destroy(goalPlayer2);
+        goalPlayer1 = Instantiate(goalPrefab, new Vector3(-11f, Random.Range(-3.38f, 4.20f), 0.0f), Quaternion.identity);
+        goalPlayer2 = Instantiate(goalPrefab, new Vector3(11f, Random.Range(-3.38f, 4.20f), 0.0f), Quaternion.identity);
 
-
-
-        //cc.enable = true
-
-
+        player1.transform.position = player1pos;
+        player2.transform.position = player2pos;
 
         Debug.Log("restarting");
     }
---
+++ PocketLeague/Assets/Scripts/GoalManager.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GoalManager : MonoBehaviour
+{
+
+    public GameManager gm;
+
+    private void Start()
+    {
+        gm = GameObject.FindObjectOfType<GameManager>();
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.tag == "Ball")
+        {
+            if (this.transform.position.x > 0)
+            {
+                gm.Goal(gm.player1);
+            }
+            else
+            {
+                gm.Goal(gm.player2);
+            }
+        }
+
+    }
+
+}
